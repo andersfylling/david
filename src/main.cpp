955bb77990d348ac34641f64a9e90b88a619cd47
anders øen fylling <fyllingz@gmail.com>
2017-04-09 21:22:28 +0200

    fixed cmake issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
-#include "../lib/Catch/single_include/catch.hpp"
+#include "floatfann.h"
 
-//
-//Main
-//
-int main(int argc, char *args[]) {
+int main()
+{
+  fann_type *calc_out;
+  fann_type input[2];
+
+  struct fann *ann = fann_create_from_file("xor_float.net");
+
+  input[0] = -1;
+  input[1] = 1;
+  calc_out = fann_run(ann, input);
+
+  printf("xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
+
+  fann_destroy(ann);
   return 0;
 }
