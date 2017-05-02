3bb4fe25f459b3ea1ca7478ae5765c13a1ff9bd8
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-02 21:53:09 +0200

    create scorenode and boardsnode

---
+++ src/BitboardsNode.h
@@ -0,0 +1,32 @@
+//
+// Created by anders on 5/2/17.
+//
+
+#ifndef CHESS_ANN_BITBOARDSNODE_H
+#define CHESS_ANN_BITBOARDSNODE_H
+
+#include "bitboard.h"
+#include "ScoreNode.h"
+class BitboardsNode {
+ private:
+  ScoreNode* scoreNode;
+  bitboard* white;
+  bitboard* whitePawns;
+  bitboard* whiteQueens;
+  bitboard* whiteKings;
+  bitboard* whiteRooks;
+  bitboard* whiteBishops;
+  bitboard* whiteKnights;
+  bitboard* black;
+  bitboard* blackPawns;
+  bitboard* blackQueens;
+  bitboard* blackKings;
+  bitboard* blackRooks;
+  bitboard* blackBishops;
+  bitboard* blackKnights;
+
+ public:
+  BitboardsNode();
+};
+
+#endif //CHESS_ANN_BITBOARDSNODE_H
--- src/stockfish_evaluation.cpp
+++ src/stockfish_evaluation.cpp
@@ -3,30 +3,30 @@
 //
 
 namespace stockfish_wrapper {
-#include "../lib/Stockfish/src/evaluate.h"
-#include "../lib/Stockfish/src/types.h"
-#include "../lib/Stockfish/src/position.h"
-
-
-int valueToInt(Value val) {
-  int score = 0;
-
-
-  // update score based on Value struct data.
-
-
-  return score;
-}
-Position nodeToPosition() {
-  Position pos;
-
-  return pos;
-}
-
-// Node pointer, or reference
-int getScoreFromNode(/* node pointe ror reference here */) {
-  auto pos = nodeToPosition(/* node pointe ror reference here */);
-  auto val = Eval::evaluate(pos);
-  return valueToInt(val);
-}
+//#include "../lib/Stockfish/src/evaluate.h"
+//#include "../lib/Stockfish/src/types.h"
+//#include "../lib/Stockfish/src/position.h"
+//
+//
+//int valueToInt(Value val) {
+//  int score = 0;
+//
+//
+//  // update score based on Value struct data.
+//  score = static_cast<int>(val);
+//
+//  return score;
+//}
+//Position nodeToPosition() {
+//  Position pos;
+//
+//  return pos;
+//}
+//
+//// ScoreNode pointer, or reference
+//int getScoreFromNode(/* node pointe ror reference here */) {
+//  auto pos = nodeToPosition(/* node pointe ror reference here */);
+//  auto val = Eval::evaluate(pos);
+//  return valueToInt(val);
+//}
 }
