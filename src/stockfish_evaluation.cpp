29c29cc75c9047c8d4f4324ac8251c0076964ed5
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-02 19:02:59 +0200

    a wrapper function

--- src/stockfish_evaluation.cpp
+++ src/stockfish_evaluation.cpp
@@ -8,8 +8,25 @@ namespace stockfish_wrapper {
 #include "../lib/Stockfish/src/position.h"
 
 
-int valueToInt() {}
-Position nodeToPosition() {}
+int valueToInt(Value val) {
+  int score = 0;
 
-int getScoreFromNode() {}
+
+  // update score based on Value struct data.
+
+
+  return score;
+}
+Position nodeToPosition() {
+  Position pos;
+
+  return pos;
+}
+
+// Node pointer, or reference
+int getScoreFromNode(/* node pointe ror reference here */) {
+  auto pos = nodeToPosition(/* node pointe ror reference here */);
+  auto val = Eval::evaluate(pos);
+  return valueToInt(val);
+}
 }
