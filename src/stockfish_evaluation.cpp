f67a7e1914604d3094da5b6dff378eb114cc928e
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-02 18:57:49 +0200

    laid basic setup

---
+++ src/stockfish_evaluation.cpp
@@ -0,0 +1,15 @@
+//
+// Created by anders on 5/2/17.
+//
+
+namespace stockfish_wrapper {
+#include "../lib/Stockfish/src/evaluate.h"
+#include "../lib/Stockfish/src/types.h"
+#include "../lib/Stockfish/src/position.h"
+
+
+int valueToInt() {}
+Position nodeToPosition() {}
+
+int getScoreFromNode() {}
+}
