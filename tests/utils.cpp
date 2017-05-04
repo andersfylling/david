daba0096c9af5548d1be3409ca2a62c319086254
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-05 00:19:08 +0200

    merging..

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -1,5 +1,5 @@
 #include "catch.h"
-#include "../src/utils.h"
+#include "../engine/include/chess_ann/utils.h"
 
 TEST_CASE("Verify that absolute path is working") {
   std::cout << utils::getAbsoluteProjectPath();
