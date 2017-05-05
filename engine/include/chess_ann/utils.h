93acc1500303e623232fbf799444b6a562126915
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-05 14:25:56 +0200

    updated utils and method in environment

--- engine/include/chess_ann/utils.h
+++ engine/include/chess_ann/utils.h
@@ -10,7 +10,6 @@
 namespace utils {
   int stoi(std::string v);
   bool bitAt(uint64_t b, uint8_t i);
-  bool utils::bitAt(uint64_t b, int i);
 
 };
 
--- engine/src/utils.cpp
+++ engine/src/utils.cpp
@@ -20,14 +20,3 @@ int utils::stoi(std::string v) {
 bool utils::bitAt(uint64_t b, uint8_t i) {
   return (b & (1 << i)) != 0;
 }
-
-/**
- * Check if a bit is set at given index.
- *
- * @param b The bits in form of a uint64_t
- * @param i the index as int
- * @return true if bit is 1 at index i
- */
-bool utils::bitAt(uint64_t b, int i) {
-  return (b & (1 << i)) != 0;
-}
