981e2a0483257282573a2b15ee6e5ff72facda5c
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-05 04:02:24 +0200

    can now compile stockfish objects

--- engine/src/stockfish_evaluation.cpp
+++ engine/src/stockfish_evaluation.cpp
@@ -1,32 +1,31 @@
-//
-// Created by anders on 5/2/17.
-//
+
+#include <chess_ann/bitboard.h>
+#include "stockfish/evaluate.h"
+#include "stockfish/types.h"
+#include "stockfish/position.h"
 
 namespace stockfish_wrapper {
-//#include "../lib/Stockfish/src/evaluate.h"
-//#include "../lib/Stockfish/src/types.h"
-//#include "../lib/Stockfish/src/position.h"
-//
-//
-//int valueToInt(Value val) {
-//  int score = 0;
-//
-//
-//  // update score based on Value struct data.
-//  score = static_cast<int>(val);
-//
-//  return score;
-//}
-//Position nodeToPosition() {
-//  Position pos;
-//
-//  return pos;
-//}
-//
-//// ScoreNode pointer, or reference
-//int getScoreFromNode(/* node pointe ror reference here */) {
-//  auto pos = nodeToPosition(/* node pointe ror reference here */);
-//  auto val = Eval::evaluate(pos);
-//  return valueToInt(val);
-//}
+using Eval::evaluate;
+
+int valueToInt(Value val) {
+  int score = 0;
+
+
+  // update score based on Value struct data.
+  score = static_cast<int>(val);
+
+  return score;
+}
+void nodeToPosition(::bitboard::gameState& n, Position& pos) {
+
+}
+
+// ScoreNode pointer, or reference
+int getScoreFromNode(::bitboard::gameState& node) {
+  Position pos;
+  nodeToPosition(node, pos);
+
+  auto val = evaluate(pos);
+  return valueToInt(val);
+}
 }
