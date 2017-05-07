759f3c14883a4fc7260ef187b4ee24f2ca0ca55d
Klingenbergen <martin.klingenberg@hig.no>
2017-05-08 01:45:41 +0200

    pgn-files can now be converted to a fen-string file. Seperated by \n

--- engine/ANNTraining/trainingdata/generateTrainingDataFromPGNFiles.py
+++ engine/ANNTraining/trainingdata/generateTrainingDataFromPGNFiles.py
@@ -7,3 +7,15 @@ pgnConverter = pgntofen.PgnToFen()
 pgnConverter.resetBoard()
 file = "PGNfiles/ficsgamesdb_201601_standard2000_nomovetimes_1462883.pgn"
 stats =  pgnConverter.pgnFile(file);
+
+stats = converter.pgnFile(file)
+
+output = open("fenstring.txt", "w")
+
+for lists in stats['succeeded']:
+    for nlist in lists:
+        for fenstring in nlist:
+            output.write(fenstring)
+            output.write('\n')
+
+output.close()
