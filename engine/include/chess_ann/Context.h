bd8f0ef539dde26bfe7f66d958a8b218b71e9e4c
Anders Øen Fylling <fyllingz@gmail.com>
2017-05-10 21:04:51 +0200

    fixed circular dependency and created context class
    Former-commit-id: a2e07267c73b9b8d86aa26366be7fc7825b65517

---
+++ engine/include/chess_ann/Context.h
@@ -0,0 +1,16 @@
+//
+// Created by anders on 5/10/17.
+//
+
+#ifndef CHESS_ANN_CONTEXT_H
+#define CHESS_ANN_CONTEXT_H
+
+
+
+class Context {
+
+};
+
+
+
+#endif //CHESS_ANN_CONTEXT_H
---
+++ engine/src/Context.cpp
@@ -0,0 +1,11 @@
+//
+// Created by anders on 5/10/17.
+//
+
+#include "Context.h"
+
+Context::Context(chess_ann::Engine* const& engine)
+    : engine(engine)
+{
+
+}
