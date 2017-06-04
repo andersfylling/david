cfb00bd6f4f8e0bd0edffe269697c05a00d1fc3b
Anders Fylling <fyllingz@gmail.com>
2017-06-04 18:53:49 +0000

    100% commenting WIP, merged info files.
    Former-commit-id: 324f2d213c0808e309402819124118a3aae957f0

--- engine/include/david/david.h
+++ engine/include/david/david.h
@@ -7,4 +7,13 @@ const std::string neuralNetworksFolder = "/engine/src/ANN/networks/";
 
 // a standard chess board layout given as a FEN string.
 const std::string FENStartPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+// information about the engine.
+namespace engineInformation {
+static const std::string PROJECT_NAME = "david"; // MUST BE THE SAME AS FOLDER NAME, WILL BE REMOVED BEFORE FIRST RELEASE!!
+static const std::string ENGINE_NAME = "david";
+static const std::string ENGINE_AUTHOR = "Anders Øen Fylling, Martin Klingenberg, Markus Jacobsen, Martin Trehjøring";
+static const std::string ENGINE_VERSION = "0.2.3"; // THIS IS JUST SOME BS NUMBER.
+static const std::string ENGINE_COPYRIGHT = ENGINE_NAME + ". Copyright (C) 2017-2017 " + ENGINE_AUTHOR + ". ";
+}
 }
