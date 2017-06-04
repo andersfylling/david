a4a68fe6ece384a5adf5e87155d806ae462ed19e
Anders Fylling <fyllingz@gmail.com>
2017-06-04 18:51:08 +0000

    100% commenting WIP
    Former-commit-id: 76e3c9277c776b970916d463e252da8f3f30c2ff

--- engine/include/david/david.h
+++ engine/include/david/david.h
@@ -2,6 +2,9 @@
 
 
 namespace david {
+// Folder to ANN networks files from project root dir.
 const std::string neuralNetworksFolder = "/engine/src/ANN/networks/";
+
+// a standard chess board layout given as a FEN string.
 const std::string FENStartPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 }
