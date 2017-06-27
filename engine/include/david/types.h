d0773cd5b18303af9232250af7eb43b16b9078d8
Anders Øen Fylling <fyllingz@gmail.com>
2017-06-27 04:13:10 +0200

    updating types.h
    Former-commit-id: c0fd511367e7a5fb7377a51736876716b67a91ab

--- engine/include/david/types.h
+++ engine/include/david/types.h
@@ -29,6 +29,10 @@ class GameTree;
 namespace david {
 namespace type {
 
+//
+// Typedefs
+//
+
 // Used by classes to communicate with other instances not directly related except on a parent level
 typedef std::shared_ptr<::david::EngineContext>       engineContext_ptr;
 
@@ -52,5 +56,49 @@ typedef std::shared_ptr<::david::bitboard::gameState> gameState_ptr;
 
 typedef uint64_t bitboard_t;  // Represents a bitboard_t
 typedef uint16_t move_t;      // Representing moves
+
+
+//
+// Types
+//
+
+
+// Each game state is represented by a struct of
+// bitboards. A tree of moves will be made up by
+struct gameState {
+  bitboard_t WhitePawn;
+  bitboard_t WhiteRook;
+  bitboard_t WhiteKnight;
+  bitboard_t WhiteBishop;
+  bitboard_t WhiteQueen;
+  bitboard_t WhiteKing;
+
+  bitboard_t BlackPawn;
+  bitboard_t BlackRook;
+  bitboard_t BlackKnight;
+  bitboard_t BlackBishop;
+  bitboard_t BlackQueen;
+  bitboard_t BlackKing;
+
+  bitboard::COLOR playerColor = bitboard::COLOR::WHITE; // Whose turn it is now
+  std::vector<std::shared_ptr<bitboard::gameState>> children;
+
+  int halfMoves = 0; // number of moves since last capture or pawn moves, otherwise incremented.
+  int fullMoves = 1; // starts at 1, increments after every time black moves.
+
+  int score = 0; // board score
+
+  int gameTreeLevel = 0;
+
+  int possibleSubMoves = 0;
+
+  bool blackQueenCastling = true;
+  bool blackKingCastling = true;
+  bool whiteQueenCastling = true;
+  bool whiteKingCastling = true;
+
+  std::weak_ptr<bitboard::gameState> weakParent; //...
+};
+
 }
 }
