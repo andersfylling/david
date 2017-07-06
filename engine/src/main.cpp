fb24408f8ea41efeb3f51603ce73bf79636b6166
Anders Øen Fylling <fyllingz@gmail.com>
2017-07-06 13:38:49 +0200

    cleaned up main
    Former-commit-id: 48049ec9fe0ba6811870564fa27547234b57ff7c

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -11,39 +11,6 @@
 
 int main (int argc, char * argv[])
 {
-  const int DEPTH = 1;
-
-  // TODO: once depth N - 1 is complete, do not regenerate everything when checking for depth N ?
-//  std::cout << "Perft results" << std::endl;
-//  std::cout << "=============" << std::endl;
-//  std::cout << "Depth" << '\t' << "Nodes" << std::endl;
-//  ::david::type::gameState_t gs;
-//  for (int i = 0; i <= DEPTH; i++) {
-//    // reset layout
-//    ::david::utils::setDefaultChessLayout(gs);
-//
-//    // get perft results
-//    std::cout << i << "\t\t" << ::david::utils::perft(i, gs) << std::endl;
-//  }
-
-
-  auto begin = std::chrono::high_resolution_clock::now();
-  uint32_t iterations = 10000;
-  ::david::type::gameState_t gs;
-  for(uint32_t i = 0; i < iterations; ++i) {
-    // reset layout
-    ::david::utils::setDefaultChessLayout(gs);
-
-    // get perft results
-    ::david::utils::perft(DEPTH, gs);
-  }
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-  std::cout << duration << "ns total, average : " << duration / iterations << "ns." << std::endl;
-
-
-  return 0;
-
   // Make sure its not some weird "cpu architecture".
   assert(sizeof(uint8_t)  == 1);
   assert(sizeof(uint16_t) == 2);
